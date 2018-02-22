3d8d6ae9aa22edbe6ed3364dae744c0189ee3a3f
samuelsson87 <samuelsson87@hotmail.com>
2018-02-22 16:35:19 +0100

    Small fixes

--- app/screens/SwipeTest.js
+++ app/screens/SwipeTest.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { StatusBar, ScrollView, Text, View, Dimensions } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
 
 import { Container } from '../components/Container'
 
@@ -13,13 +15,21 @@ class SwipeTest extends Component {
       <Container>
         <StatusBar translucent={false} barStyle="light-content" />
         <ScrollView horizontal={true} pagingEnabled>
-          <View style={{flex: 1, backgroundColor:'red', width:deviceWidth}}></View>
-          <View style={{flex: 1, backgroundColor:'yellow', width:deviceWidth}}></View>
-          <Container />
+          <View style={{flex: 1, backgroundColor:'red', width:deviceWidth, alignItems: 'center', justifyContent:'center'}}><Text style={styles.text}>1</Text></View>
+          <View style={{flex: 1, backgroundColor:'orange', width:deviceWidth, alignItems: 'center', justifyContent:'center'}}><Text style={styles.text}>2</Text></View>
+          <Container><Text style={styles.text}>3</Text></Container>
         </ScrollView>
       </Container>
     );
   }
 }
+
+const styles = EStyleSheet.create({
+  text: {
+   color: '$white',
+   fontSize: 50,
+   fontWeight: '600',
+  },
+})
 
 export default SwipeTest;
