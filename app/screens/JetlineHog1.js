341e8f866e29559b55db083ef9b00dcc3dac4947
samuelsson87 <samuelsson87@hotmail.com>
2018-03-05 16:34:27 +0100

    some working feedback

--- app/screens/JetlineHog1.js
+++ app/screens/JetlineHog1.js
@@ -1,6 +1,18 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-import { StatusBar, ScrollView, Text, View, Dimensions, TextInput,KeyboardAvoidingView, Modal, TouchableOpacity, TouchableHighlight } from "react-native";
+import {
+  StatusBar,
+  ScrollView,
+  Text,
+  View,
+  Dimensions,
+  TextInput,
+  KeyboardAvoidingView,
+  Modal,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+} from "react-native";
 import EStyleSheet from "react-native-extended-stylesheet";
 import { SwipeContainer } from "../components/Container";
 import containerStyles from "../components/Container/styles";
@@ -8,45 +20,55 @@ import containerStyles from "../components/Container/styles";
 const deviceWidth = Dimensions.get("window").width;
 
 class JetlineHog1 extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { text: '', modalVisible: false, myPopUpVisible: false };
-    
+    this.state = { text: "", modalVisible: false, myPopUpVisible: false };
   }
 
   test = () => {
-    console.warn('test1');
+    console.warn("test1");
   };
 
   // toggle = () => {
   //   this.setMyPopUpVisible(!this.state.myPopUpVisible);
   // };
 
-  test2 = (text) => {
-    this.setState({text});
-    // console.warn('test2');
-    if (text == 5) this.setMyPopUpVisible(!this.state.myPopUpVisible);
+  test2 = text => {
+    // this.setState({ text });
+    // console.warn(this.state.text);
+    if (this.state.text == 5) this.setMyPopUpVisible(!this.state.myPopUpVisible);
   };
 
-  setMyPopUpVisible(visible) {
-    this.setState({myPopUpVisible: visible});
-    // console.warn('visible'); 
-    // console.warn(this.state.myPopUpVisible);
-
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
   }
 
+  setMyPopUpVisible(visible) {
+    this.setState({ myPopUpVisible: visible });
+    // console.warn('visible');
+    // console.warn(this.state.myPopUpVisible);
+  }
 
   render() {
     return (
       <SwipeContainer>
-        <ScrollView horizontal={true} pagingEnabled showsHorizontalScrollIndicator={false}>
+        <ScrollView
+          horizontal={true}
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+        >
           <SwipeContainer>
-            <View style={[{flex:0.2, justifyContent: 'center', alignItems:'center'}]}>
+            <View
+              style={[
+                { flex: 0.2, justifyContent: "center", alignItems: "center" }
+              ]}
+            >
               <Text style={styles.headerText}>Jetline</Text>
-              <Text style={styles.subHeaderText}>Åk, känn efter och undersök</Text>
+              <Text style={styles.subHeaderText}>
+                Åk, känn efter och undersök
+              </Text>
             </View>
-            <View style={[{flex:0.8}, styles.textWrapper]}>
+            <View style={[{ flex: 0.8 }, styles.textWrapper]}>
               <Text style={[styles.greyText, styles.bullet]}>
                 {`\u2022 `}Hur får man stopp på ett berg- och dalbanetåg?
               </Text>
@@ -57,91 +79,135 @@ class JetlineHog1 extends Component {
                 {`\u2022 `}Hur fungerar en mekanisk broms?
               </Text>
               <Text style={[styles.greyText, styles.bullet]}>
-                {`\u2022 `}Ge exempel på en attraktion där mekaniska bromsar används. Vad
-                händer om det blir strömavbrott? Gå till teknikbordet och
-                undersök.
+                {`\u2022 `}Ge exempel på en attraktion där mekaniska bromsar
+                används. Vad händer om det blir strömavbrott? Gå till
+                teknikbordet och undersök.
               </Text>
               <Text style={[styles.greyText, styles.bullet]}>
-                {`\u2022 `}Kan du lyfta en Jetline-motor med en hävstång? Gå till
-                teknikbordet och försök!
+                {`\u2022 `}Kan du lyfta en Jetline-motor med en hävstång? Gå
+                till teknikbordet och försök!
               </Text>
             </View>
           </SwipeContainer>
 
-          <KeyboardAvoidingView style={containerStyles.swipeContainer} behavior="padding" keyboardVerticalOffset={50}>
-          <StatusBar translucent={false} barStyle="light-content" />
-          <View style={[{flex:0.2, justifyContent: 'center', alignItems:'center'}]}>
+          <KeyboardAvoidingView
+            style={containerStyles.swipeContainer}
+            behavior="padding"
+            keyboardVerticalOffset={50}
+          >
+            <StatusBar translucent={false} barStyle="light-content" />
+            <View
+              style={[
+                { flex: 0.2, justifyContent: "center", alignItems: "center" }
+              ]}
+            >
               <Text style={styles.headerText}>Jetline</Text>
               <Text style={styles.subHeaderText}>Mät och räkna</Text>
             </View>
-            <View style={[{flex:0.4}, styles.textWrapper]}>
+            <View style={[{ flex: 0.4 }, styles.textWrapper]}>
               <Text style={[styles.greyText, styles.bullet]}>
-                Välj en stolpe. Hur lång tid tar det för tåget att passera stolpen? Tåget är 9,2 m långt.
+                Välj en stolpe. Hur lång tid tar det för tåget att passera
+                stolpen? Tåget är 9,2 m långt.
               </Text>
               <Text style={[styles.greyText, styles.bullet]}>
                 Hur fort går tåget förbi stolpen?
               </Text>
             </View>
-            <View style={[{
-              flex: 0.4, 
-              flexDirection: 'row', 
-              alignItems:'center'}, 
-              styles.textWrapper
-              ]}>
+            <View
+              style={[
+                {
+                  flex: 0.4,
+                  flexDirection: "row",
+                  alignItems: "center"
+                },
+                styles.textWrapper
+              ]}
+            >
               <Text style={styles.greyText}>Svar: </Text>
               <TextInput
-                  keyboardType={'numeric'} 
-                  returnKeyType="done"
-                  style={{height: 40, width:70, borderRadius: 5, backgroundColor:'white', textAlign:'center', margin: 5}}
-                  onChangeText={(text) => this.test2(text)}
-                  value={this.state.text}
-                />
-                <Text style={styles.greyText}> m/s</Text>
+                keyboardType={"numeric"}
+                returnKeyType="done"
+                style={{
+                  height: 40,
+                  width: 70,
+                  borderRadius: 5,
+                  backgroundColor: "white",
+                  textAlign: "center",
+                  margin: 5
+                }}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+                onSubmitEditing={(event, text) => {
+                  this.test2(text);}}
+              />
+              <Text style={styles.greyText}> m/s</Text>
+            </View>
+            {this.state.myPopUpVisible && (
+              <View
+                style={{
+                  width: 100,
+                  height: 100,
+                  position: "absolute",
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}
+              >
+                <TouchableOpacity
+                  style={{ flex: 1 }}
+                  onPress={() => {
+                    this.setMyPopUpVisible(!this.state.myPopUpVisible);
+                  }}
+                >
+                  <Text>Rätt svar!</Text>
+                </TouchableOpacity>
               </View>
-              {this.state.myPopUpVisible &&<View style={{width:100, height:100, position:'absolute', backgroundColor: 'white', alignItems:'center', justifyContent: 'center'}}>
-              <TouchableOpacity style={{flex: 1}} onPress={() => {
-                  this.setMyPopUpVisible(!this.state.myPopUpVisible);
-                }}><Text>Rätt svar!</Text></TouchableOpacity></View>} 
-              
-          </KeyboardAvoidingView>                                                                                                       
-    
+            )}
+          </KeyboardAvoidingView>
+
           {/* this.setMyPopUpVisible(!this.state.myPopUpVisible)
           onPress={() => {
                   this.setModalVisible(!this.state.modalVisible);
                 }}> */}
           <SwipeContainer>
-            <Text style={styles.headerText}>3</Text> 
-            <View style={{marginTop: 22}}>
-        <Modal
-          animationType="slide"
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            alert('Modal has been closed.');
-          }}>
-          <View style={{marginTop: 22, marginLeft: 10}}>
-            <View>
-              <Text>Hello World!</Text>
+            <Text style={styles.headerText}>3</Text>
 
-              <TouchableHighlight
+              <Modal
+                animationType="fade"
+                transparent={true}
+                visible={this.state.modalVisible}
+                onRequestClose={() => {
+                  alert("Modal has been closed.");
+                }}
+              >
+                <TouchableOpacity
+                  style={{
+                    marginTop: 22,
+                    width:200,
+                    height:200,
+                    backgroundColor: 'white',
+                    borderRadius: 5,
+                    alignItems: "center",
+                    justifyContent: "center"
+                  }}
+                  onPress={() => {
+                    this.setModalVisible(!this.state.modalVisible);
+                  }}
+                >
+                  <View>
+                    <Text style={styles.subHeaderText}>Rätt svar!</Text>
+                  </View>
+                </TouchableOpacity>
+              </Modal>
+
+              <TouchableOpacity
                 onPress={() => {
-                  this.setModalVisible(!this.state.modalVisible);
-                }}>
-                <Text>Hide Modal</Text>
-              </TouchableHighlight>
-            </View>
-          </View>
-        </Modal>
-
-        <TouchableHighlight
-          onPress={() => {
-            this.setModalVisible(true);
-          }}>
-          <Text>Show Modal</Text>
-        </TouchableHighlight>
-      </View>
+                  this.setModalVisible(true);
+                }}
+              >
+                <Text>Show Modal</Text>
+              </TouchableOpacity>
           </SwipeContainer>
-
         </ScrollView>
       </SwipeContainer>
     );
@@ -152,25 +218,25 @@ const styles = EStyleSheet.create({
   headerText: {
     color: "$textGrey",
     fontSize: 50,
-    fontWeight: "600",
+    fontWeight: "600"
   },
   subHeaderText: {
     color: "$textGrey",
     fontSize: 20,
-    fontWeight: "800",
+    fontWeight: "800"
   },
   greyText: {
     color: "$textGrey",
     fontSize: 20,
-    fontWeight: "400",
+    fontWeight: "400"
   },
-  textWrapper:{
-    justifyContent:'center',
-    paddingHorizontal:35,
-    alignSelf: 'stretch',
+  textWrapper: {
+    justifyContent: "center",
+    paddingHorizontal: 35,
+    alignSelf: "stretch"
   },
-  bullet:{
-    paddingVertical: 15,
+  bullet: {
+    paddingVertical: 15
   }
 });
 
